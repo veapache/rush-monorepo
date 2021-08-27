4c08282bde98dabd8a5a7c7ae9f66fa034623d8c
volevol <i.fedorusov-2024@kpi.ua>
2021-08-27 14:11:14 +0300

    add CtxUser decorator to get logged in user

--- libraries/kuna-nest-prisma-volevol/src/graphql/resolvers/crud/Order/OrderResolvers.ts
+++ libraries/kuna-nest-prisma-volevol/src/graphql/resolvers/crud/Order/OrderResolvers.ts
@@ -1,5 +1,5 @@
 import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
-import { Inject, UseGuards } from '@nestjs/common';
+import { Logger,Inject, UseGuards } from '@nestjs/common';
 import { DIPrisma } from 'src/DIP';
 import { PrismaClient } from '@prisma/client';
 
@@ -11,6 +11,8 @@ import { FindUniqueOrderArgs } from './args/FindUniqueOrderArgs';
 import { GqlAuthGuard } from '../Auth/guards/GqlAuthGuard';
 import { JwtService } from '@nestjs/jwt';
 import { JwtDto } from '../../inputs/JwtDto';
+import { CtxUser } from '../Auth/decorators/CtxUser.decorator';
+import { User } from 'src/graphql/models/user.model';
 
 @UseGuards(GqlAuthGuard)
 @Resolver(Order)
@@ -31,19 +33,18 @@ export class OrderResolver {
     }
 
     @Mutation(() => Order)
-    async createOrder(@Args() args: CreateOneOrderArgs): Promise<Order> {
-        const payload: JwtDto = { userId: args.userId }
-        this.jwt.sign(payload)
-        return await this.prisma.order.create({ data: args });
+    async createOrder(@CtxUser() user: User, @Args() args: CreateOneOrderArgs): Promise<Order> {
+        Logger.log(user)
+        return await this.prisma.order.create({ data: {...args, userId: user.id}});
     }
 
     @Mutation(() => Order)
-    async updateOrder(@Args('id') id: string, @Args() args: UpdateOneOrderArgs): Promise<Order> {
+    async updateOrder(@CtxUser() user: User, @Args('id') id: string, @Args() args: UpdateOneOrderArgs): Promise<Order> {
         return this.prisma.order.update({ where: { id }, data: args });
     }
 
     @Mutation(() => Order)
-    async deleteOrder(@Args('id') id: string): Promise<Order | null> {
+    async deleteOrder(@CtxUser() user: User, @Args('id') id: string): Promise<Order | null> {
         return this.prisma.order.delete({ where: { id } });
     }
 }
